b4c1a660a26cc205286e2a798175c2f08e575d8a
Andrew Bennet <me@andrewbennet.com>
2021-09-20 21:18:10 +0100

    Initial Commit

---
+++ Shared/diffable_experimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  diffable_experimentsApp.swift
+//  Shared
+//
+//  Created by Andrew Bennet on 20/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct diffable_experimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
